9a4d51512e19a73e0a9b60d03ce3365416e715a0
deepu7d <deepanshu.9dd@gmail.com>
2025-10-19 16:22:05 +0530

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import Card from "../components/card";
 import Wrapper from "../components/wrapper";
 import { CARD_DATA } from "@/constants/index";
@@ -6,12 +7,12 @@ export default function Home() {
   return (
     <Wrapper>
       <div className="flex flex-col gap-10">
-        <div className="pt-10">
-          <h1 className="text-4xl font-bold">{"Hi, I'm Deepanshu"}</h1>
-        </div>
-        <div>
-          <h1 className="pb-10">Projects</h1>
-          <div className="flex flex-wrap gap-5 lg:gap-10">
+        <Navbar />
+        <div className="flex flex-col items-center">
+          <h1 className="mb-10 inline-block bg-neutral-800/50 p-4 text-2xl shadow-[4px_4px_0px_0px_#ffff]">
+            Projects
+          </h1>
+          <div className="flex flex-wrap justify-center gap-5 p-4 lg:gap-10">
             {CARD_DATA.map((card) => (
               <Card key={card.title} {...card} />
             ))}
